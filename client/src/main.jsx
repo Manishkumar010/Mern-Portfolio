4ab9e60dc633a5b83696c253ccc8b8baba42a02a
Manishkumar010 <mk519956@gmail.com>
2025-08-16 12:44:21 +0530

    some changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from './store/Auth.js'
+import { AuthProvider } from './store/Auth.jsx'
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
